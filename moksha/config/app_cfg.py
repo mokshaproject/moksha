a8240c3965b704e054446ff7a830629e65ff07d5
Luke Macken <lmacken@redhat.com>
2008-12-18 23:02:31 -0500

    Make the AccumulatingProfileMiddleware configurable

--- moksha/config/app_cfg.py
+++ moksha/config/app_cfg.py
@@ -30,3 +30,7 @@ base_config.sa_auth.permission_class = model.Permission
 # override this if you would like to provide a different who plugin for 
 # managing login and logout of your application
 base_config.sa_auth.form_plugin = None
+
+# To enable the repoze.profile middleware.
+# After surfing around, navigate to /__profile__ to view results.
+base_config.profile = False
