ed32b3d1236a2833d6819cf22049716412c25256
Luke Macken <lmacken@redhat.com>
2008-12-21 22:58:44 -0500

    Import the pylons.i18n.ugettext as moksha._

--- moksha/__init__.py
+++ moksha/__init__.py
@@ -1,5 +1,6 @@
-from feed import Feed
+from pylons.i18n import ugettext as _
 from paste.registry import StackedObjectProxy
+from feed import Feed
 
 # The central feed cache, used by the Feed widget.
 feed_cache = StackedObjectProxy()
