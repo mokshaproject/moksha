cee9aa3fbb602f7c65b1741ef50d60883644a8b2
Luke Macken <lmacken@redhat.com>
2008-12-23 02:59:23 -0500

    Add an initial MokshaHub, integrated with Qpid.

--- moksha/hub.py
+++ moksha/hub.py
@@ -16,35 +16,127 @@
 # Copyright 2008, Red Hat, Inc.
 # Authors: Luke Macken <lmacken@redhat.com>
 
-from tg import config
-from tw.api import Widget, JSLink, js_callback, js_function
+import qpid
+from qpid.util import connect, URL, ssl
+from qpid.client import Client
+from qpid.datatypes import Message
+from qpid.connection import Connection
 
-orbited_js = JSLink(link='http://%s:%s/static/Orbited.js' % (config['orbited_host'],
-                                                             config['orbited_port']))
+import logging
+log = logging.getLogger(__name__)
 
-class OrbitedWidget(Widget):
-    params = {
-        'onopen': 'A javascript callback for when the connection opens',
-        'onread': 'A javascript callback for when new data is read',
-        'onclose': 'A javascript callback for when the connection closes',
-    }
-    onopen = onread = onclose = js_callback('function(){}')
-    javascript = [orbited_js]
-    template = """
-        <script type="text/javascript">
-            Orbited.settings.port = %(port)s
-            Orbited.settings.hostname = '%(host)s'
-            document.domain = document.domain
-            TCPSocket = Orbited.TCPSocket
-            connect = function() {
-                conn = new TCPSocket()
-                conn.onread = ${onread}
-                conn.onopen = ${onopen}
-                conn.onclose = ${onclose}
-                conn.open('%(host)s', %(port)s)
-            }
-            $(document).ready(function() {
-                connect()
-            })
-        </script>
-    """ % {'port': config['orbited_port'], 'host': config['orbited_host']}
+class MokshaHub(object):
+
+    queues = []
+    exchanges = []
+    client = None
+    channel = None
+    amqp_spec = qpid.spec.default()
+
+    def __init__(self, broker='localhost', timeout=10):
+        """ Initialize the Moksha Hub.
+
+        `broker`
+            [amqps://][<user>[/<password>]@]<host>[:<port>]
+
+        """
+        self.set_broker(broker)
+        self.timeout = timeout
+        self.amqp_spec = qpid.spec.load(self.amqp_spec)
+        self.init_qpid_connection()
+        self.init_qpid_session()
+        self.init_qmf()
+
+    def set_broker(self, broker):
+        self.url = URL(broker)
+        self.user = self.url.password or 'guest'
+        self.password = self.url.password or 'guest'
+        self.host = self.url.host
+        if self.url.scheme == URL.AMQPS:
+            self.ssl = True
+            default_port = 5671
+        else:
+            self.ssl = False
+            default_port = 5672
+        self.port = self.url.port or default_port
+
+    def init_qpid_connection(self):
+        sock = connect(self.host, self.port)
+        if self.url.scheme == URL.AMQPS:
+            sock = ssl(sock)
+        self.conn = Connection(sock, self.amqp_spec, username=self.user,
+                               password=self.password)
+        self.conn.start(timeout=self.timeout)
+
+    def init_qpid_session(self, session_name='moksha'):
+        self.session = self.conn.session(session_name, timeout=self.timeout)
+
+    def init_qmf(self):
+        try:
+            import qmf.console
+            self.qmf = qmf.console.Session()
+            self.qmf_broker = self.qmf.addBroker(str(self.url))
+        except ImportError:
+            log.warning('qmf.console not available')
+            self.qmf = None
+
+    def close(self):
+        if not self.session.error():
+            self.session.close(timeout=self.timeout)
+        self.conn.close(timeout=self.timeout)
+        if self.qmf:
+            self.qmf.delBroker(self.qmf_broker)
+
+    def create_queue(self, queue, routing_key, exchange='amq.direct',
+                     auto_delete=False, durable=False, **kw):
+        self.session.queue_declare(queue=queue, auto_delete=auto_delete,
+                                   durable=durable, **kw)
+
+    def delete_queue(self, queue):
+        self.session.queue_delete(queue=queue)
+
+    def send_message(self, destination, message, routing_key='key',
+                     delivery_mode=2):
+        dp = self.session.delivery_properties(routing_key=routing_key,
+                                              delivery_mode=delivery_mode)
+        self.session.message_transfer(message=Message(dp, message))
+
+    def subscribe(self, queue, destination, accept_mode=1, acquire_mode=0):
+        """ Subscribe to a specific queue destination """
+        self.session.message_subscribe(queue=queue, destination=destination,
+                                       accept_mode=accept_mode,
+                                       acquire_mode=acquire_mode)
+        self.session.message_flow(destination=destination,
+                                  unit=self.session.credit_unit.message,
+                                  value=0xFFFFFFFF)
+        self.session.message_flow(destination=destination,
+                                  unit=self.session.credit_unit.byte,
+                                  value=0xFFFFFFFF)
+
+    def get(self, destination):
+        return self.session.incoming(destination).get(timeout=self.timeout)
+
+    def query(self, queue):
+        return self.session.queue_query(queue=queue)
+
+    # ability to get the latest messages from a given queue?
+
+if __name__ == '__main__':
+    sh = logging.StreamHandler()
+    log.setLevel(logging.DEBUG)
+    sh.setLevel(logging.DEBUG)
+    log.addHandler(sh)
+
+    print "Creating MokshaHub..."
+    hub = MokshaHub()
+    print "Creating queue..."
+    hub.create_queue('wtf', 'key', 'amq.topic', durable=True)
+    print "Sending message"
+    hub.send_message('wtf', 'wtf', 'wtf')
+    print "Subscribing to queue"
+    hub.subscribe('wtf', 'wtf')
+    print "Recieving message"
+    msg = hub.get('wtf')
+    print "msg = ", msg
+    hub.close()
+    print "Closed!"
