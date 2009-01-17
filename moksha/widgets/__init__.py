e28184e2ec2cdd4b2048f2211a874f761f71346e
Luke Macken <lmacken@redhat.com>
2009-01-17 17:22:20 -0500

    Add a moksha.widgets module

---
+++ moksha/widgets/__init__.py
@@ -0,0 +1,6 @@
+from live import LiveWidget
+from feed import Feed, LiveFeedWidget
+from chat import LiveChatWidget
+from flot import LiveFlotWidget
+from stomp import StompWidget
+from layout import LayoutWidget
