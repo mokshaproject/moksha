a248f8b8489e7eae5ef6a98bf58cc97920aeeb56
Luke Macken <lmacken@redhat.com>
2009-01-22 00:32:39 -0500

    Comment out moksha.api.hub.send_message.
    This method is not safe for use, as it seems to be leaking socket
    descriptors.

--- moksha/api/hub/hub.py
+++ moksha/api/hub/hub.py
@@ -1,18 +1,23 @@
-from orbited import json
-from moksha.hub import MokshaHub
+#from orbited import json
+#from moksha.hub import MokshaHub
 
-def send_message(topic, message):
-    """ Send a `message` to a specific `topic` """
+## @@ This is currently leaking socket descriptors...
 
-    # Right now we're instantiating a new connection & channel each message..
-    # we could potentially do this in the DataStreamer...
-    # each could have their own Connection...
-    # or, the MokshaHub could instantiate all DataStreamers with their
-    # own channel?
-    hub = MokshaHub()
-
-    # Automatically encode non-strings to JSON
-    if not isinstance(message, basestring):
-        message = json.encode(message)
-
-    hub.send_message(message, routing_key=topic)
+#def send_message(topic, message):
+#    """ Send a `message` to a specific `topic` """
+#
+#    # Right now we're instantiating a new connection & channel each message..
+#    # we could potentially do this in the DataStreamer...
+#    # each could have their own Connection...
+#    # or, the MokshaHub could instantiate all DataStreamers with their
+#    # own channel?
+#    hub = MokshaHub()
+#
+#    # Automatically encode non-strings to JSON
+#    if not isinstance(message, basestring):
+#        message = json.encode(message)
+#
+#    hub.send_message(message, routing_key=topic)
+#
+#    hub.stop()
+#    del(hub)
