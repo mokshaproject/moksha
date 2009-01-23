6bab6fdf2514d4429f5f36e82c5b0a4704d35375
Luke Macken <lmacken@redhat.com>
2009-01-23 02:14:37 -0500

    Add our generic MessagingHub interface for the protocol-level implementations

---
+++ moksha/hub/messaging.py
@@ -0,0 +1,30 @@
+# This file is part of Moksha.
+#
+# Moksha is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Moksha is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Moksha.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright 2008, Red Hat, Inc.
+# Authors: Luke Macken <lmacken@redhat.com>
+
+class MessagingHub(object):
+    """
+    A generic messaging hub.
+
+    This class represents the base functionality of the protocol-level hubs.
+    """
+
+    def send_message(self, topic, message):
+        raise NotImplementedError
+
+    def subscribe(self, topic):
+        raise NotImplementedError
