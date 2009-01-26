16c457faae6ace57afdc9c11c6f76c6d11a53764
Luke Macken <lmacken@redhat.com>
2009-01-26 10:55:47 -0500

    Make our trace decorator a bit more robust

--- moksha/lib/utils.py
+++ moksha/lib/utils.py
@@ -2,6 +2,8 @@ from decorator import decorator
 
 @decorator
 def trace(f, *args, **kw):
-    r = f(*args, **kw)
-    print "%s(%s, %s) = %s" % (f.func_name, args, kw, r)
+    try:
+        r = f(*args, **kw)
+    finally:
+        print "%s(%s, %s) = %s" % (f.func_name, args, kw, r)
     return r
