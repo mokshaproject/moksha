6eb77361e13bbd5de88a5fb825f2060e1c1c428e
Luke Macken <lmacken@redhat.com>
2009-01-28 15:06:08 -0500

    Add the trace decorator to the helpers module. And while we're here, make the helpers module more PEP8 compliant, and remove whitespace blobs from eclipse.

--- moksha/lib/helpers.py
+++ moksha/lib/helpers.py
@@ -1,30 +1,31 @@
-from webhelpers import date, feedgenerator, html, number, misc, text
-
-from repoze.what.predicates import  (Not, Predicate, All, Any, has_all_permissions, 
-                                    has_any_permission, has_permission, 
-                                    in_all_groups, in_any_group, in_group, 
-                                    is_user, not_anonymous)
-
 import urllib
 import uuid
-
 import re
 
+from decorator import decorator
+from webhelpers import date, feedgenerator, html, number, misc, text
+from repoze.what.predicates import  (Not, Predicate, All, Any,
+                                    has_all_permissions,
+                                    has_any_permission, has_permission,
+                                    in_all_groups, in_any_group, in_group,
+                                    is_user, not_anonymous)
+
 scrub_filter = re.compile('[^_a-zA-Z0-9-]')
+
 
 def Category(label="", apps=[], auth=[]):
     if not check_predicates(auth):
         return None
-    
+
     id = uuid.uuid4()
     css_class =  scrub_filter.sub('_', label.lower())
-    
+
     return {'label': label, 'apps': apps, 'id': id, 'css_class': css_class}
 
 def App(label="", url="", req_params={}, auth=[]):
     if not check_predicates(auth):
         return None
-    
+
     query_str = ""
     if req_params:
         query_str = "?" + urllib.urlencode(req_params)
@@ -32,6 +33,7 @@ def App(label="", url="", req_params={}, auth=[]):
     id = uuid.uuid4()
 
     return {'label': label, 'url': url + query_str, 'id': id}    
+
 
 def MokshaApp(label="", moksha_app="", req_params={}, auth=[]):
     # FIXME figure out how to pull auth info from an app
@@ -56,6 +58,7 @@ _app_config_callables = {'Category': Category,
 
 _app_config_callables.update(_safe_predicate_callables)
 
+
 def eval_app_config(config_str):
     """
     Safely evaluates application configuration strings for storing application
@@ -63,34 +66,48 @@ def eval_app_config(config_str):
     """
     return eval(config_str, {"__builtins__":None}, _app_config_callables)
 
+
 def eval_predicates(predicate_str):
     """ 
     Safely evaluates a string of predicates
     """
     return eval(predicate_str, {"__builtins__":None}, _safe_predicate_callables)
 
+
 def check_predicates(predicates):
     """
     Using the current WSGI environment evaluate a list of predicates.  
     This can only be used when inside a WSGI request
-    
+
     Return False is any one is False 
     Return True if they are all True
     """
-    
+
     from pylons import request
-    
-    if(isinstance(predicates, list) or isinstance(predicates, tuple)):
+
+    if isinstance(predicates, list) or isinstance(predicates, tuple):
         for p in predicates:
             if not p.eval_with_environ(request.environ):
                 return False
         return True
-    
-    if(isinstance(predicates, Predicate)):
+
+    if isinstance(predicates, Predicate):
         return predicates.eval_with_environ(request.environ)
-    
+
     return False
+
 
 def eval_and_check_predicates(predicate_str):
     p = eval_predicates(predicate_str)
     return check_predicates(p)
+
+
+@decorator
+def trace(f, *args, **kw):
+    """ A useful decorator for debugging method parameters and return values """
+    r = None
+    try:
+        r = f(*args, **kw)
+    finally:
+        print "%s(%s, %s) = %s" % (f.func_name, args, kw, r)
+    return r
