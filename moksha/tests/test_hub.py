bd135945e2906cd6597a2ac7773a66155f066572
Luke Macken <lmacken@redhat.com>
2009-01-29 18:23:34 -0500

    Start a simple hub test module

--- moksha/tests/test_hub.py
+++ moksha/tests/test_hub.py
@@ -1,29 +1,16 @@
 """Test Moksha's Hub """
 
-#from nose.tools import eq_, assert_true
-#from moksha.hub import MokshaHub
-#
-#class TestHub:
-#
-#    def setUp(self):
-#        self.hub = MokshaHub()
-#
-#    def tearDown(self):
-#        self.hub.close()
-#
-#    def test_creating_queue(self):
-#        self.hub.create_queue('test')
-#        eq_(len(self.hub.queues), 1)
-#
-#    def test_delete_queue(self):
-#        """ Test deleting a queue """
-#    def test_subscription(self):
-#        """ Test subscribing to a queue """
-#    def test_unsubscribing(self):
-#        """ Test unsubscribing to a queue """
-#    def test_sending_message(self):
-#        """ Test sending a simple message """
-#    def test_receiving_message(self):
-#        """ Test receiving a message """
-#    def test_query(self):
-#        """ Test querying queues """
+from nose.tools import eq_, assert_true
+from moksha import hub
+
+class TestHub:
+
+    def setUp(self):
+        self.hub = moksha.hub.MokshaHub()
+
+    def tearDown(self):
+        self.hub.close()
+
+    def test_hub_creation(self):
+        assert_true(self.hub)
+        eq_(self.hub.topics, {})
