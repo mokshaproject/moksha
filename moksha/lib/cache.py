9f1dda5f09db2453166c0a35615815b03d7e1ef8
Luke Macken <lmacken@redhat.com>
2009-02-17 19:10:22 -0500

    Gracefully handle not having python-memcached installed

--- moksha/lib/cache.py
+++ moksha/lib/cache.py
@@ -1,10 +1,22 @@
-import memcache
+import logging
+log = logging.getLogger(__name__)
+
 from moksha.exc import CacheBackendException
 
 class Cache(object): 
     """ A memcached-specific caching interface """
 
     def __init__(self, url, timeout=None, prefix=None):
+        self.create_memcached_cache()
+
+    def create_memcached_cache(self):
+        try:
+            import memcache
+        except ImportError:
+            log.warning('Cannot import the `memcache` module.  Install the '
+                        'python-memcached package to enable Mokshas memcached '
+                        'integration.')
+            return
         self.mc = memcache.Client([url])
         self.timeout = timeout or 300
         self.prefix = prefix
