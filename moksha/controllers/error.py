a7e033d5e59f7d268b221e9793dc2e0155b7e9c6
Luke Macken <lmacken@redhat.com>
2009-02-28 01:41:33 -0500

    Bring our error controller up to date with the latest TG2 quickstart

--- moksha/controllers/error.py
+++ moksha/controllers/error.py
@@ -1,7 +1,7 @@
+import tg
 import os.path
-
 import paste.fileapp
-from tg import request
+
 from pylons.controllers.util import forward
 from pylons.middleware import error_document_template, media_path
 
@@ -17,25 +17,13 @@ class ErrorController(BaseController):
     ErrorDocuments middleware in your config/middleware.py file.
     """
 
-    def document(self):
+    @tg.expose('genshi:moksha.templates.error')
+    def document(self, *args, **kwargs):
         """Render the error document"""
-        resp = request.environ.get('pylons.original_response')
-        page = error_document_template % \
-            dict(prefix=request.environ.get('SCRIPT_NAME', ''),
-                 code=request.params.get('code', resp.status_int),
-                 message=request.params.get('message', resp.body))
-        return page
-
-    def img(self, id):
-        """Serve stock images"""
-        return self._serve_file(os.path.join(media_path, 'img', id))
-
-    def style(self, id):
-        """Serve stock stylesheets"""
-        return self._serve_file(os.path.join(media_path, 'style', id))
-
-    def _serve_file(self, path):
-        """Call Paste's FileApp (a WSGI application) to serve the file
-        at the specified path
-        """
-        return forward(paste.fileapp.FileApp(path))
+        resp = tg.request.environ.get('pylons.original_response')
+        default_message = ("<p>We're sorry but we weren't able to process "
+        " this request.</p>")
+        values = dict(prefix=tg.request.environ.get('SCRIPT_NAME', ''),
+                 code=tg.request.params.get('code', resp.status_int),
+                 message=tg.request.params.get('message', default_message))
+        return values
