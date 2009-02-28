427dab842e2d8aea1610c3e23d792119dc60c94b
Luke Macken <lmacken@redhat.com>
2009-02-28 01:48:59 -0500

    Update our customized jQuery ui widgets

--- moksha/widgets/jquery_ui_theme.py
+++ moksha/widgets/jquery_ui_theme.py
@@ -1,5 +1,13 @@
-from tw.api import Widget, CSSLink
+"""
+:mod:`moksha.widgets.jquery_ui_theme` - jQuery UI Theme
+=======================================================
 
-class JQueryUITheme(Widget):
-    css = [CSSLink(link='/css/jquery-ui/ui.theme.css', modname=__name__)]
-    template = ''
+.. moduleauthor:: Luke Macken <lmacken@redhat.com>
+"""
+
+from tw.api import Widget, CSSLink, CSSLink
+
+ui_theme_css = CSSLink(link='/css/jquery-ui/ui.theme.css', modname=__name__)
+ui_base_css = CSSLink(link='/css/jquery-ui/ui.base.css',
+                      css=[ui_theme_css],
+                      modname=__name__)
