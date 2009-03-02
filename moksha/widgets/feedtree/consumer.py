d39c770671932a45b1107892f8f13c637c9c75a6
Luke Macken <lmacken@redhat.com>
2009-03-02 00:18:06 -0500

    Pull the feed tree's controller, consumer, and widgets out into their own.

---
+++ moksha/widgets/feedtree/consumer.py
@@ -0,0 +1,92 @@
+# This file is part of Moksha.
+#
+# Moksha is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Moksha is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Moksha.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright 2008, Red Hat, Inc.
+
+"""
+:mod:`moksha.widgets.feedtree.consumer` - The feed consumer
+===========================================================
+
+The :class:`MokshaFeedConsumer` listens to all messages on the ``feeds`` topic.
+If the ``action`` entry in the message header contains a valid command, it will
+execute it.  Most of the time it will perform a specific action, such as getting
+entries for a specific feed, and will send the data to the specified ``topic``.
+
+.. moduleauthor:: Luke Macken <lmacken@redhat.com>
+"""
+
+import logging
+
+from tg import config
+from shove import Shove
+from orbited import json
+
+from moksha.api.hub import Consumer
+
+log = logging.getLogger('moksha.hub')
+
+class MokshaFeedConsumer(Consumer):
+
+    topic = 'feeds'
+
+    def __init__(self):
+        super(MokshaFeedConsumer, self).__init__()
+        self.feed_storage = Shove(config['feed_cache'], compress=True)
+
+    def consume(self, message):
+        log.debug('MokshaFeedConsumer.consume(%s)' % message)
+        action = message['headers'].get('action')
+        if action and hasattr(self, action):
+            getattr(self, action)(message)
+        else:
+            # new feed found?
+            pass
+
+    def get_feed(self, message):
+        url = message['body']
+        feed = self.feed_storage[url][1]
+        entries = []
+        for entry in feed['entries']:
+            entries.append({
+                'title': entry.get('title'),
+                'isLazy': False,
+                'isFolder': False,
+                'rootVisible': False,
+                'key': "%s::%s" % (url, entry.get('title', '').replace(' ','')),
+                })
+        self.send_message(message['headers']['topic'],
+                json.encode({'entries': entries, 'key': url,
+                             'action': 'get_feed'}))
+
+    def get_entry(self, message, **kw):
+        key = message['body']
+        if '::' in key:
+            url, title = key.split('::')
+            for entry in self.feed_storage[url][1]['entries']:
+                content = entry.get('content', entry.get('summary'))
+                if isinstance(content, list):
+                    if isinstance(content[0], dict) and \
+                       content[0].has_key('value'):
+                        content = content[0]['value']
+                content = """
+                    <blockquote><h3><a href="%s">%s</a></h3><br/>%s</blockquote>
+                """ % (entry.get('link', url), entry['title'], content)
+                if entry['title'].replace(' ', '') == title:
+                    self.send_message(message['headers']['topic'],
+                            json.encode({'content': content,
+                                         'action': 'get_entry'}))
+            raise Exception("Cannot find entry by title: %s" % title)
+        else:
+            raise Exception("Invalid entry key: %r" % key)
---
+++ moksha/widgets/feedtree/controllers/feed.py
@@ -0,0 +1,134 @@
+# This file is part of Moksha.
+#
+# Moksha is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Moksha is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Moksha.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright 2008, Red Hat, Inc.
+
+"""
+The Moksha Feed Tree Controller
+
+.. moduleauthor:: Luke Macken <lmacken@redhat.com>
+"""
+
+import moksha
+import logging
+
+from tg import expose, tmpl_context
+from pylons import cache
+from orbited import json
+
+from moksha.lib.base import Controller
+from moksha.widgets.feedtree import feed_entries_tree, moksha_feedreader
+
+log = logging.getLogger('moksha.hub')
+
+class FeedController(Controller):
+
+    @expose('mako:moksha.templates.widget')
+    def index(self, *args, **kw):
+        tmpl_context.widget = moksha_feedreader
+        return dict(options={})
+
+    @expose()
+    def init_tree(self, key, fresh=False, **kw):
+        c = cache.get_cache('feeds')
+        if fresh:
+            return self._get_feed_titles(fresh=fresh)
+        else:
+            return c.get_value(key='feed_titles',
+                               createfunc=self._get_feed_titles,
+                               expiretime=3600)
+
+    def _get_feed_titles(self, fresh=False):
+        results = []
+        for feed in moksha.feed_storage.keys():
+            if not feed:
+                raise Exception('None feed?!')
+            if fresh:
+                feed_data = moksha.feed_cache.fetch(feed)
+            else:
+                timestamp, feed_data = moksha.feed_storage[feed]
+            if not feed_data:
+                log.debug("no feed_data, refetching")
+                feed_data = moksha.feed_cache.fetch(feed)
+                if not feed_data:
+                    log.debug("NO FEED DATA AFTER FRESH FETCH!!!!")
+                    continue
+            channel = feed_data.get('channel')
+            if not channel:
+                continue
+            title = channel.get('title')
+            results.append({
+                'title': title,
+                'key': feed,
+                'isLazy': False,
+                'isFolder': True,
+                })
+        return json.encode(results)
+
+    @expose()
+    def get_feed(self, key, *args, **kw):
+        if '::' in key:
+            url, title = key.split('::')
+            for entry in moksha.feed_storage[url][1]['entries']:
+                content = entry.get('content', entry.get('summary'))
+                content = '<span style="line-height:100%%;">%s</span>' % content
+                if entry['title'] == title:
+                    return json.encode([{'title': content, 'isLazy': False}])
+            raise Exception("Cannot find entry by title: %s" % title)
+
+        feed = moksha.feed_storage[key][1]
+        entries = []
+        for entry in feed['entries']:
+            entries.append({
+                'title': entry.get('title'),
+                'isLazy': True,
+                'isFolder': True,
+                'key': "%s::%s" % (key, entry.get('title')),
+                })
+        return json.encode(entries)
+
+    @expose('mako:moksha.templates.widget')
+    def get_entries(self, key, **kw):
+        tmpl_context.widget = feed_entries_tree
+        feed = moksha.feed_storage[key][1]
+        entries = []
+        for entry in feed['entries']:
+            entries.append({
+                'title': entry.get('title'),
+                'isLazy': False,
+                'isFolder': False,
+                'rootVisible': False,
+                'key': "%s::%s" % (key, entry.get('title','').replace(' ','')),
+                })
+        return dict(options={'tree_children': entries})
+
+    @expose()
+    def get_entry(self, key, **kw):
+        if '::' in key:
+            url, title = key.split('::')
+            for entry in moksha.feed_storage[url][1]['entries']:
+                content = entry.get('content', entry.get('summary'))
+                if isinstance(content, list):
+                    if isinstance(content[0], dict) and \
+                       content[0].has_key('value'):
+                        content = content[0]['value']
+                content = """
+                    <blockquote><h3><a href="%s">%s</a></h3><br/>%s</blockquote>
+                """ % (entry.get('link', url), entry['title'], content)
+                if entry['title'].replace(' ', '') == title:
+                    return content
+            raise Exception("Cannot find entry by title: %s" % title)
+        else:
+            raise Exception("Invalid entry key: %r" % key)
