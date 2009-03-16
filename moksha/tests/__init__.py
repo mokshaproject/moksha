2e19f48a6efa8c2b80f80a17c6052e44b864d290
Luke Macken <lmacken@redhat.com>
2009-03-16 11:58:37 -0400

    Bring our test suite up to speed with the latest TurboGears2 quickstart

--- moksha/tests/__init__.py
+++ moksha/tests/__init__.py
@@ -1,38 +1,74 @@
-"""Pylons application test package
+# -*- coding: utf-8 -*-
+"""Unit and functional test suite for Moksha """
 
-When the test runner finds and executes tests within this directory,
-this file will be loaded to setup the test environment.
-
-It registers the root directory of the project in sys.path and
-pkg_resources, in case the project hasn't been installed with
-setuptools. It also initializes the application via websetup (paster
-setup-app) with the project's test.ini configuration file.
-"""
-import os
+from os import path
 import sys
 
-import pkg_resources
-import webtest
-import paste.script.appinstall
+from tg import config
 from paste.deploy import loadapp
+from paste.script.appinstall import SetupCommand
 from routes import url_for
+from webtest import TestApp
+from nose.tools import eq_
+
+from moksha import model
+
+__all__ = ['setup_db', 'teardown_db', 'TestController', 'url_for']
+
+def setup_db():
+    engine = config['pylons.app_globals'].sa_engine 
+    model.init_model(engine)
+    model.metadata.create_all(engine)
+
+def teardown_db():
+    engine = config['pylons.app_globals'].sa_engine
+    model.metadata.drop_all(engine)
+
+def setup_app(application): 
+    # Loading the application:
+    conf_dir = config.here
+    wsgiapp = loadapp('config:test.ini#%s' % application,
+                      relative_to=conf_dir)
+    app = TestApp(wsgiapp)
+    # Setting it up:
+    test_file = path.join(conf_dir, 'test.ini')
+    cmd = SetupCommand('setup-app')
+    cmd.run([test_file])
+    return app
 
 __all__ = ['url_for', 'TestController']
 
-here_dir = os.path.dirname(os.path.abspath(__file__))
-conf_dir = os.path.dirname(os.path.dirname(here_dir))
-
-sys.path.insert(0, conf_dir)
-pkg_resources.working_set.add_entry(conf_dir)
-pkg_resources.require('Paste')
-pkg_resources.require('PasteScript')
-
-test_file = os.path.join(conf_dir, 'test.ini')
-cmd = paste.script.appinstall.SetupCommand('setup-app')
-cmd.run([test_file])
 
 class TestController(object):
+    """
+    Base functional test case for the controllers.
 
-    def __init__(self, *args, **kwargs):
-        wsgiapp = loadapp('config:test.ini', relative_to=conf_dir)
-        self.app = webtest.TestApp(wsgiapp)
+    The Moksha application instance (``self.app``) set up in this test 
+    case (and descendants) has authentication disabled, so that developers can
+    test the protected areas independently of the :mod:`repoze.who` plugins
+    used initially. This way, authentication can be tested once and separately.
+
+    Check Moksha for the repoze.who integration tests.
+
+    This is the officially supported way to test protected areas with
+    repoze.who-testutil (http://code.gustavonarea.net/repoze.who-testutil/).
+
+    """
+
+    application_under_test = 'main_without_authn'
+
+    def setUp(self):
+        # Loading the application:
+        conf_dir = config.here
+        wsgiapp = loadapp('config:test.ini#%s' % self.application_under_test,
+                          relative_to=conf_dir)
+        self.app = TestApp(wsgiapp)
+        # Setting it up:
+        test_file = path.join(conf_dir, 'test.ini')
+        cmd = SetupCommand('setup-app')
+        cmd.run([test_file])
+
+    def tearDown(self):
+        # Cleaning up the database:
+        engine = config['pylons.app_globals'].sa_engine
+        model.metadata.drop_all(engine)
