d6c2182767a2ec69cd95d27eed7478f1eb05fca7
Luke Macken <lmacken@redhat.com>
2009-03-16 13:14:24 -0400

    Fix our container test cases

--- moksha/widgets/container/tests/test_container.py
+++ moksha/widgets/container/tests/test_container.py
@@ -1,10 +1,10 @@
 from tw.api import Widget
-from moksha.widgets.container import JQueryMbContainer
+from moksha.widgets.container import MokshaContainer
 
 class TestContainer:
 
     def setUp(self):
-        self.w = JQueryMbContainer('test')
+        self.w = MokshaContainer('test')
 
     def test_render_widget(self):
         assert '<div id="test" ' in self.w()
@@ -12,13 +12,12 @@ class TestContainer:
     def test_widget_content(self):
         """ Ensure we can render a container with another widget """
         class MyWidget(Widget):
-            template = 'Hello World!'
+            template = """
+                Hello World!
+            """
         assert 'Hello World!' in self.w(content=MyWidget('mywidget'))
 
     def test_container_classes(self):
-        assert 'class="container skin3 stikynote draggable resizable"'\
-                in self.w(**dict(skin3=True, stikynote=True,
+        rendered = self.w(**dict(skin3=True, stikynote=True,
                                  draggable=True, resizable=True))
-        assert 'class="container"' \
-                in self.w(**dict(skin3=True, stikynote=True,
-                                 draggable=True, resizable=True))
+        assert 'class="containerPlus draggable resizable"' in rendered, rendered
