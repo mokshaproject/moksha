45d3461a22807a6210f1f3d7cb5ea008b41a9baa
Luke Macken <lmacken@redhat.com>
2009-06-27 20:32:20 -0400

    Deprecate moksha.api.widgets.iframe and move it to moksha.widgets.iframe

--- moksha/api/widgets/iframe.py
+++ moksha/api/widgets/iframe.py
@@ -1,26 +1,4 @@
-# This file is part of Moksha.
-# Copyright (C) 2008-2009  Red Hat, Inc.
-# 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-# 
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-from tw.api import Widget
-
-class IFrameWidget(Widget):
-    params = ['id', 'url']
-    template = """
-      <iframe id="${id}" src="${url}" width="100%" height="100%">
-        <p>Your browser does not support iframes.</p>
-      </iframe>
-    """
-    engine_name = 'mako'
+from moksha.widgets.iframe import IFrameWidget
+from moksha.lib.helpers import deprecation
+deprecation("The `moksha.api.widgets.iframe` module has been deprecated.  "
+            "Please use `moksha.widgets.iframe` instead")
