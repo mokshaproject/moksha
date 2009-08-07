6030a00e25e7ae88abb2852171b4becf4c5b4208
Luke Macken <lmacken@redhat.com>
2009-08-07 14:35:56 -0400

    Add a moksha docs app

--- moksha/apps/docs/pavement.py
+++ moksha/apps/docs/pavement.py
@@ -1,19 +1,30 @@
 from paver.easy import *
-from paver.setuputils import setup
+from paver.setuputils import (setup, find_package_data, find_packages,
+                              install_distutils_tasks)
+from moksha.lib.paver_tasks import *
 
-setup(
-    name="moksha.docs",
-    version="1.0",
-    url="http://moksha.fedorahosted.org",
-    description="Moksha Docs App",
-    author="Luke Macken",
-    author_email="lmacken@redhat.com",
-    packages=[],
-    install_requires=["Moksha"],
-    entry_points="""
+install_distutils_tasks()
 
-    [moksha.application]
-    docs = moksha.apps.docs.docs:docs
-
-    """,
+options(
+    setup=Bunch(
+        name="moksha.docs",
+        version="1.0",
+        release="1",
+        url="http://moksha.fedorahosted.org",
+        description="Moksha Docs App",
+        long_description="TODO.",
+        license='AGPLv3',
+        rpm_name='moksha-apps-docs',
+        author="Luke Macken",
+        author_email="lmacken@redhat.com",
+        packages=find_packages(),
+        package_data=find_package_data(),
+        install_requires=["Moksha"],
+        namespace_packages=['moksha'],
+        entry_points={
+            'moksha.application': (
+                'docs = moksha.apps.docs.docs:docs',
+            ),
+        }
+    ),
 )
