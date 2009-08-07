fc0b679921395fc6b3f5d8e3869016216b04e0c3
Luke Macken <lmacken@redhat.com>
2009-08-07 14:36:50 -0400

    Add a moksha menus app

--- moksha/apps/menus/pavement.py
+++ moksha/apps/menus/pavement.py
@@ -1,37 +1,33 @@
 from paver.easy import *
-from paver.setuputils import setup, find_package_data, find_packages
+from paver.setuputils import (setup, find_package_data, find_packages,
+                              install_distutils_tasks)
+from moksha.lib.paver_tasks import *
 
-setup(
-    name="moksha.menus",
-    version="0.1",
-    url="http://moksha.fedorahosted.org",
-    description="Moksha Menus App",
-    author="Luke Macken",
-    author_email="lmacken@redhat.com",
-    packages=find_packages(),
-    package_data=find_package_data(),
-    namespace_packages=['moksha'],
-    install_requires=["Moksha"],
-    entry_points="""
+install_distutils_tasks()
 
-    [moksha.menu]
-    default_menu = moksha.apps.menus:MokshaDefaultMenu
-
-    [moksha.application]
-    menu = moksha.apps.menus.controllers:MokshaMenuController
-
-    """,
+options(
+    setup=Bunch(
+        name="moksha.menus",
+        version="0.1",
+        release="1",
+        url="http://moksha.fedorahosted.org",
+        description="Moksha Menus App",
+        license="AGPLv3",
+        long_description="",
+        author="Luke Macken",
+        author_email="lmacken@redhat.com",
+        rpm_name='moksha-menus',
+        packages=find_packages(),
+        package_data=find_package_data(),
+        namespace_packages=['moksha'],
+        install_requires=["Moksha"],
+        entry_points={
+            'moksha.menu': (
+                'default_menu = moksha.apps.menus:MokshaDefaultMenu',
+            ),
+            'moksha.application': (
+                'menu = moksha.apps.menus.controllers:MokshaMenuController',
+            ),
+        }
+    ),
 )
-
-@task
-def rpm():
-    sh('rm -fr dist')
-    sh('paver sdist')
-    sh('mv dist/* ~/rpmbuild/SOURCES/')
-    sh('cp *.spec ~/rpmbuild/SPECS/')
-    sh('rpmbuild -ba ~/rpmbuild/SPECS/moksha-menus.spec')
-
-@task
-@needs(['rpm'])
-def reinstall():
-    sh('sudo rpm -ivh --replacefiles --replacepkgs ~/rpmbuild/RPMS/noarch/moksha-menus-0.1-1.noarch.rpm')
