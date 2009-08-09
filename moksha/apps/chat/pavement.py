c08444613bcf0504aa2527b44299f66f466f95d6
Luke Macken <lmacken@redhat.com>
2009-08-09 00:25:47 -0400

    Add a moksha.chat app

---
+++ moksha/apps/chat/pavement.py
@@ -0,0 +1,33 @@
+from paver.easy import *
+from paver.setuputils import (setup, find_package_data, find_packages,
+                              install_distutils_tasks)
+from moksha.lib.paver_tasks import *
+
+install_distutils_tasks()
+
+options(
+    setup=Bunch(
+        name="moksha.chat",
+        version="0.1",
+        release="1",
+        url="http://moksha.fedorahosted.org",
+        description="Moksha Chat App",
+        long_description="",
+        author="Luke Macken",
+        author_email="lmacken@redhat.com",
+        license="AGPLv3",
+        rpm_name='moksha-apps-chat',
+        packages=find_packages(),
+        package_data=find_package_data(),
+        namespace_packages=['moksha'],
+        install_requires=["Moksha"],
+        entry_points={
+            'moksha.application': (
+                'chat = moksha.apps.chat.controllers:ChatController',
+            ),
+            'moksha.widget': (
+                'chat = moksha.apps.chat:LiveChatWidget',
+            ),
+        }
+    ),
+)
