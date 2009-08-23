6191a5afd390cbd7e892e73af959d8d4cd68f52b
Luke Macken <lmacken@redhat.com>
2009-08-23 00:08:12 -0400

    Make our IFrameWidget more configurable

--- moksha/widgets/iframe.py
+++ moksha/widgets/iframe.py
@@ -24,10 +24,15 @@
 from tw.api import Widget
 
 class IFrameWidget(Widget):
-    params = ['id', 'url']
+    params = ['id', 'url', 'title', 'height', 'width']
     template = """
-      <iframe id="${id}" src="${url}" width="100%" height="100%">
+      <h1>${title}</h1>
+      <iframe id="${id}" src="${url}" width="${width}" height="${height}">
         <p>Your browser does not support iframes.</p>
       </iframe>
     """
+    title = ''
+    height = width = '100%'
     engine_name = 'mako'
+
+iframe_widget = IFrameWidget('iframe_widget')
