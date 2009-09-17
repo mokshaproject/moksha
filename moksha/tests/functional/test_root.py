94a610cf8dea7c392ccf49b1c87a2bd66f506c13
Luke Macken <lmacken@redhat.com>
2009-09-17 19:52:26 -0400

    Add a few initial basic test cases to assert a few things

--- moksha/tests/functional/test_root.py
+++ moksha/tests/functional/test_root.py
@@ -30,26 +30,28 @@ from nose.tools import assert_true
 
 from moksha.tests import TestController
 
-
 class TestRootController(TestController):
+
     def test_index(self):
-        response = self.app.get('/')
-        #msg = 'TurboGears 2 is rapid web application development toolkit '\
-        #      'designed to make your life easier.'
-        # You can look for specific strings:
-        #assert_true(msg in response)
+        resp = self.app.get('/')
+        assert '[ Moksha ]' in resp
 
-        # You can also access a BeautifulSoup'ed response in your tests
-        # (First run $ easy_install BeautifulSoup 
-        # and then uncomment the next two lines)  
+    def test_jquery_injection(self):
+        """ Ensure jQuery is getting injected on our main dashboard """
+        resp = self.app.get('/')
+        assert 'jquery' in resp
 
-        #links = response.html.findAll('a')
-        #print links
-        #assert_true(links, "Mummy, there are no links here!")
+    def test_global_resources(self):
+        """ Ensure we are getting our global resources injected """
+        resp = self.app.get('/')
+        assert 'moksha_csrf_token' in resp
 
-    #def test_secc_with_anonymous(self):
-    #    """Anonymous users must not access the secure controller"""
-    #    # We should get a redirect to login
-    #    resp = self.app.get('/secc', status=302)
-    #    print resp
-    #    assert "/login?came_from=http%3A%2F%2Flocalhost%2Fsecc" in resp
+    def test_menu(self):
+        """ Ensure that our default menu is being created """
+        resp = self.app.get('/')
+        assert 'buildMenu' in resp
+
+    def test_tcpsocket(self):
+        """ Ensure our TCP socket is getting injected """
+        resp = self.app.get('/')
+        assert 'TCPSocket' in resp
