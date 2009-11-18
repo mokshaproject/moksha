0c5e8c1f1a7aa5be576ca9a39b2fe4a5ffb62fb6
Luke Macken <lmacken@redhat.com>
2009-11-18 09:23:45 -0500

    Make our moksha_socket configurable.
    This doesn't feel right, and should probably be done in a better way...
    It will also cause problems if this module gets loaded before the
    TG/Pylons config gets loaded (ie, before the middleware).  In TurboGears
    2.1a, I believe this problem is resolved.

--- moksha/api/widgets/live/__init__.py
+++ moksha/api/widgets/live/__init__.py
@@ -1,28 +1,31 @@
 # This file is part of Moksha.
 # Copyright (C) 2008-2009  Red Hat, Inc.
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import pylons
+
 from live import LiveWidget, subscribe_topics, unsubscribe_topics
-
-# At the moment we're using the StompWidget as our primary publish/subscribe
-# messaging standard.  If we want to change the default to an AMQP widget
-# down the road, we would change it here so we have a consistent API to
-# develop against.
-
-#from moksha.api.widgets.stomp import StompWidget
-#moksha_socket = StompWidget
-
+from moksha.api.widgets.stomp import StompWidget
 from moksha.api.widgets.amqp import AMQPSocket
-moksha_socket = AMQPSocket
+
+livesocket_backend = pylons.config.get('moksha.livesocket.backend', 'stomp').lower()
+if livesocket_backend == 'stomp':
+    moksha_socket = StompWidget
+elif livesocket_backend == 'amqp':
+    moksha_socket = AMQPSocket
+else:
+    raise Exception("Unknown `moksha.livesocket.backend` %r.  Available backends: "
+                    "stomp, amqp" % livesocket_backend)
+del(livesocket_backend)
