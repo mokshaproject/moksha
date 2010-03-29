c42045294955ee91fdcea9b0c0b28c74f9e5bc0b
John (J5) Palmieri <johnp@redhat.com>
2010-03-29 13:32:39 -0400

    add ui_widget_js to the moksha_ui_tabs_js link since it isn't being pulled in

--- moksha/api/widgets/containers/tabbedcontainer.py
+++ moksha/api/widgets/containers/tabbedcontainer.py
@@ -1,27 +1,28 @@
 # This file is part of Moksha.
 # Copyright (C) 2008-2010  Red Hat, Inc.
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from tw.jquery.ui_tabs import JQueryUITabs, jquery_ui_tabs_js
+from tw.jquery.ui import ui_widget_js
 from tw.api import Widget, JSLink, js_function
 from tw.forms import FormField
 from pylons import config, request
 from repoze.what import predicates
 from moksha.lib.helpers import eval_app_config, ConfigWrapper, when_ready
 
-moksha_ui_tabs_js = JSLink(modname='moksha', filename='public/javascript/ui/moksha.ui.tabs.js', javascript=[jquery_ui_tabs_js])
+moksha_ui_tabs_js = JSLink(modname='moksha', filename='public/javascript/ui/moksha.ui.tabs.js', javascript=[jquery_ui_tabs_js, ui_widget_js])
 
 import urllib
 
