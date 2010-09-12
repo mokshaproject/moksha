7afb20e0f6a144c21eaecbb0aa284ad8403e9cfb
Luke Macken <lmacken@redhat.com>
2010-09-12 00:47:45 -0400

    Update our auth model from the latest TG2.1 quickstart

--- moksha/model/auth.py
+++ moksha/model/auth.py
@@ -1,104 +1,148 @@
+# -*- coding: utf-8 -*-
 # This file is part of Moksha.
 # Copyright (C) 2008-2010  Red Hat, Inc.
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Auth* related model.
+
+This is where the models used by :mod:`repoze.who` and :mod:`repoze.what` are
+defined.
+"""
 
 import os
-import md5
-import sha
 from datetime import datetime
+import sys
 try:
     from hashlib import sha1
 except ImportError:
-    from sha import sha as sha1
+    sys.exit('ImportError: No module named hashlib\n'
+             'If you are on python2.4 this library is not part of python. '
+             'Please install it. Example: easy_install hashlib')
 
-from tg import config
 from sqlalchemy import Table, ForeignKey, Column
-from sqlalchemy.types import String, Unicode, UnicodeText, Integer, DateTime, \
-                             Boolean, Float
-from sqlalchemy.orm import relation, backref, synonym
+from sqlalchemy.types import Unicode, Integer, DateTime
+from sqlalchemy.orm import relation, synonym
 
 from moksha.model import DeclarativeBase, metadata, DBSession
 
+__all__ = ['User', 'Group', 'Permission']
+
+
+#{ Association tables
+
 
 # This is the association table for the many-to-many relationship between
-# groups and permissions.
+# groups and permissions. This is required by repoze.what.
 group_permission_table = Table('tg_group_permission', metadata,
     Column('group_id', Integer, ForeignKey('tg_group.group_id',
-        onupdate="CASCADE", ondelete="CASCADE")),
+        onupdate="CASCADE", ondelete="CASCADE"), primary_key=True),
     Column('permission_id', Integer, ForeignKey('tg_permission.permission_id',
-        onupdate="CASCADE", ondelete="CASCADE"))
+        onupdate="CASCADE", ondelete="CASCADE"), primary_key=True)
 )
 
 # This is the association table for the many-to-many relationship between
-# groups and members - this is, the memberships.
+# groups and members - this is, the memberships. It's required by repoze.what.
 user_group_table = Table('tg_user_group', metadata,
     Column('user_id', Integer, ForeignKey('tg_user.user_id',
-        onupdate="CASCADE", ondelete="CASCADE")),
+        onupdate="CASCADE", ondelete="CASCADE"), primary_key=True),
     Column('group_id', Integer, ForeignKey('tg_group.group_id',
-        onupdate="CASCADE", ondelete="CASCADE"))
+        onupdate="CASCADE", ondelete="CASCADE"), primary_key=True)
 )
 
-# auth model
+
+#{ The auth* model itself
+
 
 class Group(DeclarativeBase):
-    """An ultra-simple group definition.
     """
+    Group definition for :mod:`repoze.what`.
+
+    Only the ``group_name`` column is required by :mod:`repoze.what`.
+
+    """
+
     __tablename__ = 'tg_group'
 
+    #{ Columns
+
     group_id = Column(Integer, autoincrement=True, primary_key=True)
+
     group_name = Column(Unicode(16), unique=True, nullable=False)
+
     display_name = Column(Unicode(255))
+
     created = Column(DateTime, default=datetime.now)
+
+    #{ Relations
+
     users = relation('User', secondary=user_group_table, backref='groups')
 
+    #{ Special methods
+
     def __repr__(self):
-        return '<Group: name=%s>' % self.group_name
+        return '<Group: name=%r>' % self.group_name
 
     def __unicode__(self):
         return self.group_name
 
+    #}
 
-#
+
 # The 'info' argument we're passing to the email_address and password columns
 # contain metadata that Rum (http://python-rum.org/) can use generate an
 # admin interface for your models.
-#
 class User(DeclarativeBase):
-    """Reasonably basic User definition. Probably would want additional
-    attributes.
+    """
+    User definition.
+
+    This is the user definition used by :mod:`repoze.who`, which requires at
+    least the ``user_name`` column.
+
     """
     __tablename__ = 'tg_user'
 
+    #{ Columns
+
     user_id = Column(Integer, autoincrement=True, primary_key=True)
+
     user_name = Column(Unicode(16), unique=True, nullable=False)
+
     email_address = Column(Unicode(255), unique=True, nullable=False,
                            info={'rum': {'field':'Email'}})
+
     display_name = Column(Unicode(255))
+
     _password = Column('password', Unicode(80),
                        info={'rum': {'field':'Password'}})
+
     created = Column(DateTime, default=datetime.now)
 
+    #{ Special methods
+
     def __repr__(self):
-        return '<User: email="%s", display name="%s">' % (
-                self.email_address, self.display_name)
+        return '<User: name=%r, email=%r, display=%r>' % (
+                self.user_name, self.email_address, self.display_name)
 
     def __unicode__(self):
         return self.display_name or self.user_name
 
+    #{ Getters and setters
+
     @property
     def permissions(self):
+        """Return a set with all permissions granted to the user."""
         perms = set()
         for g in self.groups:
             perms = perms | set(g.permissions)
@@ -106,104 +150,90 @@ class User(DeclarativeBase):
 
     @classmethod
     def by_email_address(cls, email):
-        """A class method that can be used to search users
-        based on their email addresses since it is unique.
-        """
-        return DBSession.query(cls).filter(cls.email_address==email).first()
+        """Return the user object whose email address is ``email``."""
+        return DBSession.query(cls).filter(cls.email_address == email).first()
 
     @classmethod
     def by_user_name(cls, username):
-        """A class method that permits to search users
-        based on their user_name attribute.
-        """
-        return DBSession.query(cls).filter(cls.user_name==username).first()
+        """Return the user object whose user name is ``username``."""
+        return DBSession.query(cls).filter(cls.user_name == username).first()
 
     def _set_password(self, password):
-        """encrypts password on the fly using the encryption
-        algo defined in the configuration
-        """
-        algorithm = self.get_encryption_method()
-        self._password = self.__encrypt_password(algorithm, password)
+        """Hash ``password`` on the fly and store its hashed version."""
+        # Make sure password is a str because we cannot hash unicode objects
+        if isinstance(password, unicode):
+            password = password.encode('utf-8')
+        salt = sha1()
+        salt.update(os.urandom(60))
+        hash = sha1()
+        hash.update(password + salt.hexdigest())
+        password = salt.hexdigest() + hash.hexdigest()
+        # Make sure the hashed password is a unicode object at the end of the
+        # process because SQLAlchemy _wants_ unicode objects for Unicode cols
+        if not isinstance(password, unicode):
+            password = password.decode('utf-8')
+        self._password = password
 
     def _get_password(self):
-        """returns password
-        """
+        """Return the hashed version of the password."""
         return self._password
 
     password = synonym('_password', descriptor=property(_get_password,
                                                         _set_password))
 
-    def __encrypt_password(self, algorithm, password):
-        """Hash the given password with the specified algorithm. Valid values
-        for algorithm are 'md5' and 'sha1'. All other algorithm values will
-        be essentially a no-op."""
-        hashed_password = password
-
-        if isinstance(password, unicode):
-            password_8bit = password.encode('UTF-8')
-        else:
-            password_8bit = password
-
-        if "md5" == algorithm:
-            hashed_password = md5.new(password_8bit).hexdigest()
-        elif "sha1" == algorithm:
-            hashed_password = sha.new(password_8bit).hexdigest()
-        elif "salted_sha1" == algorithm:
-            salt = sha1()
-            salt.update(os.urandom(60))
-            hash = sha1()
-            hash.update(password_8bit + salt.hexdigest())
-            hashed_password = salt.hexdigest() + hash.hexdigest()
-
-        # make sure the hased password is an UTF-8 object at the end of the
-        # process because SQLAlchemy _wants_ a unicode object for Unicode columns
-        if not isinstance(hashed_password, unicode):
-            hashed_password = hashed_password.decode('UTF-8')
-
-        return hashed_password
-
-    def get_encryption_method(self):
-        """returns the encryption method from the config
-        If None is set, or auth is disabled this will return None
-        """
-        auth_system = config.get('sa_auth', None)
-        if auth_system is None:
-            # if auth is not activated in the config we should warn
-            # the admin through the logs... and return None
-            return None
-
-        return auth_system.get('password_encryption_method', None)
+    #}
 
     def validate_password(self, password):
-        """Check the password against existing credentials.
-        this method _MUST_ return a boolean.
-
-        @param password: the password that was provided by the user to
-        try and authenticate. This is the clear text version that we will
-        need to match against the (possibly) encrypted one in the database.
-        @type password: unicode object
         """
-        algorithm = self.get_encryption_method()
-        if "salted_sha1" == algorithm:
-            hashed_pass = sha1()
-            hashed_pass.update(password + self.password[:40])
+        Check the password against existing credentials.
 
-            return self.password[40:] == hashed_pass.hexdigest()
+        :param password: the password that was provided by the user to
+            try and authenticate. This is the clear text version that we will
+            need to match against the hashed one in the database.
+        :type password: unicode object.
+        :return: Whether the password is valid.
+        :rtype: bool
 
-        else:
-            return self.password == self.__encrypt_password(algorithm, password)
+        """
+        hash = sha1()
+        if isinstance(password, unicode):
+            password = password.encode('utf-8')
+        hash.update(password + str(self.password[:40]))
+        return self.password[40:] == hash.hexdigest()
 
 
 class Permission(DeclarativeBase):
-    """A relationship that determines what each Group can do
     """
+    Permission definition for :mod:`repoze.what`.
+
+    Only the ``permission_name`` column is required by :mod:`repoze.what`.
+
+    """
+
     __tablename__ = 'tg_permission'
 
+    #{ Columns
+
     permission_id = Column(Integer, autoincrement=True, primary_key=True)
-    permission_name = Column(Unicode(16), unique=True, nullable=False)
+
+    permission_name = Column(Unicode(63), unique=True, nullable=False)
+
     description = Column(Unicode(255))
+
+    #{ Relations
+
     groups = relation(Group, secondary=group_permission_table,
                       backref='permissions')
 
+    #{ Special methods
+
+    def __repr__(self):
+        return '<Permission: name=%r>' % self.permission_name
+
     def __unicode__(self):
         return self.permission_name
+
+    #}
+
+
+#}
