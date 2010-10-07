76b3467fec05c769e3299b8759a0efe1097d054a
Luke Macken <lmacken@redhat.com>
2010-10-07 18:34:11 -0400

    Cosmetic tweak to our demo widget

--- demo/demo/widget.py
+++ demo/demo/widget.py
@@ -2,8 +2,8 @@ from moksha.api.widgets.live import LiveWidget
 
 class HelloWorldWidget(LiveWidget):
     topic = "helloworld"
-    onmessage = "$('#data').append(json.msg).append('<br/>');"
     template = """
         <b>Hello World Widget</b>
         <div id="data"/>
     """
+    onmessage = "$('#data').append(json.msg).append('<br/>');"
