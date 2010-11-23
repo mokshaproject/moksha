74a2e0825f3029b6d3a3164221d11fbdf551b8d1
Luke Macken <lmacken@redhat.com>
2010-11-23 15:03:10 -0500

    Allow people to send messages in our basic HelloWorldWidget demo

--- demo/demo/widgets/live.py
+++ demo/demo/widgets/live.py
@@ -4,7 +4,19 @@ class HelloWorldWidget(LiveWidget):
     topic = "helloworld"
     template = """
         <b>Hello World Widget</b>
+        <form onsubmit="return send_msg()">
+            <input name="text" id="text"/>
+        </form>
+
         <ul id="data"/>
+
+        <script>
+            function send_msg() {
+                moksha.send_message('helloworld', {'msg': $('#text').val()});
+                $('#text').val('');
+                return false;
+            }
+        </script>
     """
     onmessage = """
         $('<li/>').text(json.msg).prependTo('#data');
