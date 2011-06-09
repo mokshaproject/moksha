4a06be3bd963deaad2a35c2623e03231c7994406
Ralph Bean <ralph.bean@gmail.com>
2011-06-09 12:10:38 -0400

    Looser requirement on ~/.moksha/ctl.conf

--- moksha/ctl/core/config.py
+++ moksha/ctl/core/config.py
@@ -11,28 +11,21 @@ def load_config(fname="~/.moksha/ctl.conf"):
     config_d = {
         'VENV': 'moksha',
         'APPS_DIR': 'moksha/apps',
+        'SRC_DIR': os.getcwd(),
     }
 
     config = ConfigParser.ConfigParser()
 
     fname = os.path.expanduser(fname)
-    if not os.path.exists(fname):
-        raise ValueError("No such file %s" % fname)
+    if os.path.exists(fname):
+        print "Reading config from", fname
+        with open(fname) as f:
+            config.readfp(f)
 
-    print "Reading config from", fname
-    with open(fname) as f:
-        config.readfp(f)
-
-    if not config.has_section('moksha'):
-        raise ValueError("config file must have 'moksha' section")
-
-    # Check for required fields
-    if not config.has_option('moksha', 'SRC_DIR'):
-        raise ValueError("config file must have a SRC_DIR")
-
-    # Extract all defined fields
-    for key in ['SRC_DIR', 'VENV', 'APPS_DIR']:
-        if config.has_option('moksha', key):
-            config_d[key] = config.get('moksha', key)
+        if config.has_section('moksha'):
+            # Extract all defined fields
+            for key in ['SRC_DIR', 'VENV', 'APPS_DIR']:
+                if config.has_option('moksha', key):
+                    config_d[key] = config.get('moksha', key)
 
     return config_d
