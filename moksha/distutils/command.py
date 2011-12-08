d7ecb2e403aafddae351e3f7f823fcb4db4e185d
Ralph Bean <ralph.bean@gmail.com>
2011-12-07 22:54:34 -0500

    Comments are good.

--- moksha/distutils/command.py
+++ moksha/distutils/command.py
@@ -5,7 +5,10 @@ from tg import config
 from paste.deploy import appconfig
 from paste.deploy.converters import asbool
 
+# If 'moksha' doesn't show up anywhere, we likely haven't loaded any config.
 if not any(['moksha' in key for key in config.keys()]):
+
+    # Try a couple guesses at where a config file might live.
     config = None
     for guess in ['production.ini', 'development.ini']:
         try:
@@ -15,10 +18,15 @@ if not any(['moksha' in key for key in config.keys()]):
         except OSError as e:
             print str(e)
 
+    # If none of them worked, then we quit.
     if not config:
         print "Couldn't find a valid config file."
         sys.exit(1)
 
+    # Otherwise, export our newly loaded config back to tg for the rest of
+    # moksha to consume.  This is the crux -- moksha widgets need to know
+    # whether to load as tw1 widgets or as tw2 widgets when they're slated to
+    # have their resources archived.
     tg.config = config
 
 
