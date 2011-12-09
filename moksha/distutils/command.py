33dbba73a335e8abced514f31dba8b4deaee66bd
Ralph Bean <ralph.bean@gmail.com>
2011-12-09 11:22:16 -0500

    Somewhat more intelligent guessing for archive_moksha_resources.

--- moksha/distutils/command.py
+++ moksha/distutils/command.py
@@ -9,30 +9,42 @@ from paste.deploy.converters import asbool
 import tw2.core.command
 import tw.core.command
 
-# If 'moksha' doesn't show up anywhere, we likely haven't loaded any config.
-if not any(['moksha' in key for key in config.keys()]):
+def find_moksha_config():
+    # Check for config files in the following locations
+    possible_locations = [os.getcwd(), '/etc/moksha/']
 
-    # Try a couple guesses at where a config file might live.
-    config = None
-    for guess in ['production.ini', 'development.ini']:
-        try:
-            config = appconfig('config:%s' % guess, relative_to=os.getcwd())
-            print "Found", guess
-            break
-        except OSError as e:
-            print str(e)
+    # Furthermore, look for these config filenames first
+    preferred_names = ['build.ini', 'production.ini', 'development.ini']
 
-    # If none of them worked, then we quit.
-    if not config:
+    for location in possible_locations:
+        filenames = [f for f in os.listdir(location) if f.endswith('.ini')]
+
+        for preferred_name in preferred_names:
+            if preferred_name in filenames:
+                return os.path.join(location, preferred_name)
+
+    return None
+
+def load_moksha_config():
+    filename = find_moksha_config()
+
+    if not filename:
         print "Couldn't find a valid config file."
         sys.exit(1)
 
-    # Otherwise, export our newly loaded config back to tg for the rest of
+    print "Using", filename
+    config = appconfig('config:%s' % filename)
+
+    # Export our newly loaded config back to tg for the rest of
     # moksha to consume.  This is the crux -- moksha widgets need to know
     # whether to load as tw1 widgets or as tw2 widgets when they're slated to
     # have their resources archived.
     tg.config = config
+    return config
 
+# If 'moksha' doesn't show up anywhere, we likely haven't loaded any config.
+if not any(['moksha' in key for key in config.keys()]):
+    config = load_moksha_config()
 
 if asbool(config.get('moksha.use_tw2', False)):
     archive_moksha_resources = tw2.core.command.archive_tw2_resources
