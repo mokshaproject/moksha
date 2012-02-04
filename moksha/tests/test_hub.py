4b280ce437f78cdd00ce990238d2de5fbe7c0766
Ralph Bean <rbean@redhat.com>
2012-02-04 10:35:58 -0500

    Added some hub-functionality tests.

--- moksha/tests/test_hub.py
+++ moksha/tests/test_hub.py
@@ -1,6 +1,6 @@
 # This file is part of Moksha.
 # Copyright (C) 2008-2010  Red Hat, Inc.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -16,8 +16,13 @@
 """Test Moksha's Hub """
 
 import moksha
+
+from time import sleep
+from uuid import uuid4
+
 from moksha.hub import MokshaHub
-from nose.tools import eq_, assert_true
+from nose.tools import eq_, assert_true, assert_false
+
 
 class TestHub:
 
@@ -30,3 +35,30 @@ class TestHub:
     def test_hub_creation(self):
         assert_true(self.hub)
         eq_(self.hub.topics, {})
+
+    def test_hub_send_recv(self):
+        "Test that we can send a message and receive it."
+
+        secret = str(uuid4())
+        messages_received = []
+
+        def callback(json):
+            messages_received.append(json.body[1:-1])
+
+        self.hub.subscribe(topic="foobar", callback=callback)
+        self.hub.send_message(topic="foobar", message=secret)
+        sleep(1)
+        eq_(messages_received, [secret])
+
+    def test_hub_no_subscription(self):
+        "Test that if we don't receive messages we're not subscribed for."
+
+        secret = str(uuid4())
+        messages_received = []
+
+        def callback(json):
+            messages_received.append(json.body[1:-1])
+
+        self.hub.send_message(topic="foobar", message=secret)
+        sleep(1)
+        eq_(messages_received, [])
