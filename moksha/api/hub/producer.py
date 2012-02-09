cef023b510936ab2b223019b587ad9c88c1103a1
Ralph Bean <rbean@redhat.com>
2012-02-08 22:20:21 -0500

    Deprecating  API.

--- moksha/api/hub/producer.py
+++ moksha/api/hub/producer.py
@@ -16,7 +16,73 @@
 :mod:`moksha.api.hub.producer - The Moksha Producer API
 =======================================================
 """
-from moksha.api.streams import *
 
-Producer = DataStream
-PollingProducer = PollingDataStream
+import logging
+
+from datetime import timedelta
+from sqlalchemy.orm import sessionmaker
+from twisted.internet.task import LoopingCall
+
+from moksha.lib.helpers import create_app_engine
+
+log = logging.getLogger('moksha.hub')
+
+class Producer(object):
+    """ The parent Producer class. """
+
+    def __init__(self, hub):
+        self.hub = hub
+        self.log = log
+
+        # If the stream specifies an 'app', then setup `self.engine` to
+        # be a SQLAlchemy engine for that app, along with a configured DBSession
+        app = getattr(self, 'app', None)
+        self.engine = self.DBSession = None
+        if app:
+            self.engine = create_app_engine(app)
+            self.DBSession = sessionmaker(bind=self.engine)()
+
+    def send_message(self, topic, message):
+        try:
+            self.hub.send_message(topic, message)
+        except Exception, e:
+            log.error('Cannot send message: %s' % e)
+
+    def stop(self):
+        if hasattr(self, 'hub') and self.hub:
+            self.hub.close()
+        if hasattr(self, 'DBSession') and self.DBSession:
+            self.DBSession.close()
+
+
+class PollingProducer(Producer):
+    """ A self-polling producer
+
+    This class represents a data stream that wakes up at a given frequency,
+    and calls the :meth:`poll` method.
+    """
+    frequency = None # Either a timedelta object, or the number of seconds
+    now = False
+
+    def __init__(self, hub):
+        super(PollingProducer, self).__init__(hub)
+        self.timer = LoopingCall(self.poll)
+        if isinstance(self.frequency, timedelta):
+            seconds = self.frequency.seconds + \
+                    (self.frequency.days * 24 * 60 * 60) + \
+                    (self.frequency.microseconds / 1000000.0)
+        else:
+            seconds = self.frequency
+        log.debug("Setting a %s second timer" % seconds)
+        self.timer.start(seconds, now=self.now)
+
+    def poll(self):
+        raise NotImplementedError
+
+    def stop(self):
+        super(PollingProducer, self).stop()
+        try:
+            if hasattr(self, 'timer'):
+                self.timer.stop()
+        except Exception, e:
+            self.log.warn(e)
