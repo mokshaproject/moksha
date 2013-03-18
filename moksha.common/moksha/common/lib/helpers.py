cf589141f9d40974489cbf551653f1344124c537
Luke Macken <lmacken@redhat.com>
2013-03-18 11:45:00 -0700

    We no longer throw MokshaConfigNotFound

--- moksha.common/moksha/common/lib/helpers.py
+++ moksha.common/moksha/common/lib/helpers.py
@@ -25,8 +25,6 @@ import ConfigParser
 from decorator import decorator
 from kitchen.text.converters import to_unicode as kitchen_unicode
 
-from moksha.common.exc import MokshaConfigNotFound
-
 log = logging.getLogger(__name__)
 scrub_filter = re.compile('[^_a-zA-Z0-9-]')
 
@@ -43,56 +41,7 @@ def trace(f, *args, **kw):
     return r
 
 
-# TODO -- remove this and just get it from pypi
-try:
-    from collections import defaultdict
-except:
-    # A pure Python version of Python 2.5's defaultdict
-    # http://code.activestate.com/recipes/523034/
-    # by Jason Kirtland
-    class defaultdict(dict):
-        def __init__(self, default_factory=None, *a, **kw):
-            if (default_factory is not None and
-                not hasattr(default_factory, '__call__')):
-                raise TypeError('first argument must be callable')
-            dict.__init__(self, *a, **kw)
-            self.default_factory = default_factory
-
-        def __getitem__(self, key):
-            try:
-                return dict.__getitem__(self, key)
-            except KeyError:
-                return self.__missing__(key)
-
-        def __missing__(self, key):
-            if self.default_factory is None:
-                raise KeyError(key)
-            self[key] = value = self.default_factory()
-            return value
-
-        def __reduce__(self):
-            if self.default_factory is None:
-                args = tuple()
-            else:
-                args = self.default_factory,
-            return type(self), args, None, None, self.items()
-
-        def copy(self):
-            return self.__copy__()
-
-        def __copy__(self):
-            return type(self)(self.default_factory, self)
-
-        def __deepcopy__(self, memo):
-            import copy
-            return type(self)(self.default_factory,
-                              copy.deepcopy(self.items()))
-
-        def __repr__(self):
-            return 'defaultdict(%s, %s)' % (self.default_factory,
-                                            dict.__repr__(self))
-
-
+# TODO --
 def cache_rendered_data(data):
     """ A method to cache ``data`` with the current request path as the key.
 
@@ -157,7 +106,6 @@ def get_moksha_config_path():
              'controlling app is fully configured')
 
     return None
-    # raise MokshaConfigNotFound('Cannot find moksha configuration file!')
 
 
 def get_moksha_dev_config():
@@ -339,4 +287,3 @@ def listify(something):
         return not isinstance(something, list) and [something] or something
     else:
         return []
-
