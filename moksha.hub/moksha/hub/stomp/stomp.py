559fe3e65b7a22cca03f82c3114127c8e1e6bdfd
Ralph Bean <rbean@redhat.com>
2014-04-07 14:51:24 -0400

    Copyright stuff.

--- moksha.hub/moksha/hub/stomp/stomp.py
+++ moksha.hub/moksha/hub/stomp/stomp.py
@@ -1,5 +1,5 @@
 # This file is part of Moksha.
-# Copyright (C) 2008-2010  Red Hat, Inc.
+# Copyright (C) 2008-2014  Red Hat, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,6 +14,7 @@
 # limitations under the License.
 #
 # Authors: Luke Macken <lmacken@redhat.com>
+#          Ralph Bean <rbean@redhat.com>
 
 import stomper
 import logging
