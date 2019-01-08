f3686460390ccd76bc9051b6942212be57f7f500
Ralph Bean <rbean@redhat.com>
2019-01-08 15:42:56 -0500

    Make SNI optional.
    Turns out python-service-identity (and python-characteristic) are not available
    in epel7 and it will be a pain to get them reviewed and packaged.
    
    Make this optional so we can still build there.

--- moksha.hub/moksha/hub/stomp/stomp.py
+++ moksha.hub/moksha/hub/stomp/stomp.py
@@ -86,9 +86,16 @@ class StompHubExtension(MessagingHubExtension, ClientFactory):
                 with open(crt) as cert_file:
                     client_cert = ssl.PrivateCertificate.loadPEM(
                         key_file.read() + cert_file.read())
-            # connect SSL/TLS with SNI support (https://twistedmatrix.com/trac/ticket/5190)
-            # This requires service_identity module: https://pypi.python.org/pypi/service_identity
-            ssl_context = ssl.optionsForClientTLS(host.decode('utf-8'), clientCertificate=client_cert)
+
+            try:
+                import service_identity
+                # connect SSL/TLS with SNI support (https://twistedmatrix.com/trac/ticket/5190)
+                # This requires service_identity module: https://pypi.python.org/pypi/service_identity
+                ssl_context = ssl.optionsForClientTLS(host.decode('utf-8'), clientCertificate=client_cert)
+            except ImportError:
+                log.warn("Connecting without SNI support due to absence of service_identity module.")
+                ssl_context = client_cert.options()
+
             reactor.connectSSL(host, int(port), self, ssl_context)
         else:
             log.info("connecting unencrypted to %r %r %r" % (
